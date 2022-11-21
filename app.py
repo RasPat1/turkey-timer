6c4ae8b38cd77475f72c6f60aef9db2ae258fa4e
Rasesh Patel <raspat1@gmail.com>
2022-11-20 23:23:30 -0500

    Remove that local host run command from app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from turkey_math import do_turkey_math
 app = Flask(__name__)
 
 
-@app.route('/form')
+@app.route('/')
 def form():
   return render_template('./form.html')
 
@@ -12,13 +12,10 @@ def form():
 @app.route('/data', methods=['POST', 'GET'])
 def data():
   if request.method == 'GET':
-    return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    return f"The URL /data is accessed directly. Try going to '/' to submit form"
   if request.method == 'POST':
     form_data = request.form
     output = do_turkey_math(**form_data)
     if not output:
       return render_template('invalid_data.html')
     return render_template('data.html', form_data=output)
-
-
-app.run(host='localhost', port=5000)
